c8237a11e47c4685b6191af4b46d734e504622b8
aukcode <oyvinauk@stud.ntnu.no>
2016-11-07 18:33:03 +0100

    prøver meg fram med PHP

---
+++ testphp.php
@@ -0,0 +1,18 @@
+<html>
+  <head>
+    <title>PHPside</title>
+  </head>
+  <body>
+    <?php
+    // comments are double slash
+
+    echo "<p>Data processed</>";
+
+    date_default_timezone_Set('UTC')
+
+    echo date('h:i:s:u a, l F jS Y e')
+
+    ?>
+
+  </body>
+</html>
