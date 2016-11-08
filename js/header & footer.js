7e93bd66b9e54018f95f5fd261abe2bc0be9636a
Endre M.A. Ulberg <endremau@gmail.com>
2016-11-08 13:40:42 +0100

    Div ting
    bare fiksa noe shit

--- js/header & footer.js
+++ js/header & footer.js
@@ -1,9 +1,11 @@
 function head_foot() {
 	let header = document.createElement("header");
 	let footer = document.createElement("footer");
-	/*document.getElementsByTagName("html").insertBefore(header,"body");*/
-	let x = document.getElementsByTagName("html")
+	let x = document.getElementById("top")
+	let body = document.getElementById("body")
 	x.appendChild(footer);
+	x.insertBefore(header,body);
+	
 }
 
 head_foot()
